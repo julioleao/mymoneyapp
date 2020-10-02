ca9016da7c9ebee7d7e8373c1205cef56aa8b628
julioleao <julio0023@live.com>
2020-10-02 17:51:54 -0300

    Tratada as menssagens de erro

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -2,13 +2,17 @@ const restful = require("node-restful");
 const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, required: true },
+  name: { type: String, required: [true, "Informe o nome do crédito"] },
+  value: { type: Number, required: [true, "Informe o valor do crédito"] },
 });
 
 const debitSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  name: { type: String, required: [true, "Informe o nome do débito"] },
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, "Informe o valor do débito"],
+  },
   status: {
     type: String,
     required: false,
@@ -18,7 +22,10 @@ const debitSchema = new mongoose.Schema({
 });
 
 const billingCycleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: [true, "Informe o nome do Ciclo de Pagamento"],
+  },
   month: { type: Number, min: 1, max: 12, required: true },
   year: { type: Number, min: 1970, max: 2100, required: true },
   credits: [creditSchema],
