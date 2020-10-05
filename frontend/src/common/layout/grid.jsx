97445cb4f60c3e587e290834ef2cc66eba6cf98e
julioleao <julio0023@live.com>
2020-10-05 18:40:57 -0300

    Criado os componentes grid e row para melhor responsividade da interface.

--- frontend/src/common/layout/grid.jsx
+++ frontend/src/common/layout/grid.jsx
@@ -6,9 +6,9 @@ export default class Grid extends Component {
     let classes = '';
 
     if (col[0]) classes += `col-xs-${col[0]}`;
-    if (col[1]) classes += `col-sm-${col[1]}`;
-    if (col[2]) classes += `col-md-${col[2]}`;
-    if (col[3]) classes += `col-lg-${col[3]}`;
+    if (col[1]) classes += ` col-sm-${col[1]}`;
+    if (col[2]) classes += ` col-md-${col[2]}`;
+    if (col[3]) classes += ` col-lg-${col[3]}`;
 
     return classes;
   }
