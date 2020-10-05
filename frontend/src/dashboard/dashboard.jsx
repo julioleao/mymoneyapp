59897be4dbd76c81ee65fc240a7aa8baba4f4a2d
julioleao <julio0023@live.com>
2020-10-05 18:44:43 -0300

    Adicionado Action e Reducer ao componente Dashboard, fazendo a comunicação com o backend através do axios.

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { getSummary } from './dashboardActions';
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
+import ValueBox from '../common/widget/valueBox';
+import Row from '../common/layout/row';
 
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getSummary();
+  }
+
   render() {
+    const { credit, debt } = this.props.summary;
     return (
       <div>
         <ContentHeader title='Dashboard' small='Versão 1.0' />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <ValueBox
+              col='12 4'
+              color='green'
+              icon='bank'
+              value={`R$ ${credit}`}
+              text='Total de Créditos'
+            />
+            <ValueBox
+              col='12 4'
+              color='red'
+              icon='credit-card'
+              value={`R$ ${debt}`}
+              text='Total de Débitos'
+            />
+            <ValueBox
+              col='12 4'
+              color='blue'
+              icon='money'
+              value={`R$ ${credit - debt}`}
+              text='Valor Consolidado'
+            />
+          </Row>
+        </Content>
       </div>
     );
   }
 }
-
-export default Dashboard;
+const mapStateToProps = (state) => ({ summary: state.dashboard.summary });
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ getSummary }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
